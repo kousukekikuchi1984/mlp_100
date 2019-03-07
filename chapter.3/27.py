97d5571c921483b4d0157f292d498d14b63844f2
kousukekikuchi1984 <kousukekikuchi1984@gmail.com>
2019-03-07 16:42:32 +0900

    completed 27.py

--- chapter.3/27.py
+++ chapter.3/27.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import re
 import json
 
@@ -10,21 +11,27 @@ def uk_text(path):
 
 def get_basic_information(uktext):
     regex = (r"^{{基礎情報 国"
-r"([\s\S]*)"
-r"^}}")
+        r"([\s\S]*)"
+        r"^}}")
     reg = re.compile(regex, re.MULTILINE)
     finds = reg.findall(uktext)
     return finds[0]
 
 def remove_markup(text):
     text = re.sub(r"\^{2, 10}", "", text)
+    regex = r"\[\[(?:[^|]*?\|)??([^|]*?)\]\]"
+    regx = re.compile(regex, re.MULTILINE+re.VERBOSE)
+    text = regx.sub(r"\1", text)
+    return text
 
 uktext = uk_text("jawiki-country.json")
 text = get_basic_information(uktext)
+text = remove_markup(text)
+#
 reg = re.compile(r"\|(.+) = (.+)", re.MULTILINE + re.VERBOSE)
 results = reg.findall(text)
 jdict = {}
 for r in results:
-    jdict[r[0]] = re.sub(r"\^{2, 10}", "", r[1])
+    jdict[r[0]] = r[1]
+    print("%s : %s" % (r[0], r[1]))
 
-print(jdict)
